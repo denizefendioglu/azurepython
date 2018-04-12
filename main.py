5333cda3dbebfa3894df9d9ac1a396f0378ea783
Deniz Efendioglu <deniz@kunaiconsulting.com>
2018-04-12 16:27:32 -0700

    Fixed try catch

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def exec_script():
 		dirname = os.path.dirname(__file__)
 		output = subprocess.run([os.path.join(dirname, 'scripts', 'cmd.sh')], stdout=subprocess.PIPE).stdout.decode('utf-8')
 		return 'Exec output: {}'.format(output)
-	except Exception, e:
+	except Exception as e:
 		return 'Exec error! {}'.format(e)
 
 if __name__ == '__main__':
