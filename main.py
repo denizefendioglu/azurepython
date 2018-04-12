42a23ed444029d4e8f535be23f4ff1fa836f610d
Deniz Efendioglu <deniz@kunaiconsulting.com>
2018-04-12 16:23:07 -0700

    Added a try/catch around exec

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def hello_world():
 
 @app.route('/exec')
 def exec_script():
-	dirname = os.path.dirname(__file__)
-	output = subprocess.run([os.path.join(dirname, 'scripts', 'cmd.sh')], stdout=subprocess.PIPE).stdout.decode('utf-8')
-	return 'Exec output: {}'.format(output)
+	try:
+		dirname = os.path.dirname(__file__)
+		output = subprocess.run([os.path.join(dirname, 'scripts', 'cmd.sh')], stdout=subprocess.PIPE).stdout.decode('utf-8')
+		return 'Exec output: {}'.format(output)
+	except Exception, e:
+		return 'Exec error! {}'.format(e)
 
 if __name__ == '__main__':
   app.run()
